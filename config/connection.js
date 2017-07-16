c152d8d61e02624ad6f8916699781b16ad092456
tarek <T_adam731@yahoo.com>
2017-07-16 14:13:12 -0400

    change database for local host

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL){
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
     password: 'letterxbox12',
     database: 'burger_db',
